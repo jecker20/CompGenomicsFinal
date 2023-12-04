4edc645f978b8e806c2973d111e14e738bec77fb
alexurbia <112350354+alexurbia@users.noreply.github.com>
2023-12-04 17:11:40 -0500

    Update naiveKmerIndex.py with match popping

--- naiveKmerIndex.py
+++ naiveKmerIndex.py
@@ -25,7 +25,7 @@ def make_dataset_index(reads, k):
 def exact_matches(read, peptides, protIndexDict, synProts, k):
     matches = defaultdict()
     for i in range(len(peptides)):
-        for n in protIndexDict.keys():
+        for n in protIndexDict.copy().keys():
             currIdx = protIndexDict[n]
             if peptides[i][:k] in currIdx:
                 name = n
@@ -34,6 +34,7 @@ def exact_matches(read, peptides, protIndexDict, synProts, k):
                 else:
                     if peptides[i] == synProts[name]:
                         matches[str(read) + '-' + str(i + 1)] = n
+                        protIndexDict.pop(n)
     return matches
 
 # Main function called, seperated for benchmarking
